f624f0cf05daef584ab74b64cc8ab0fbd8077411
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-02-07 15:20:31 +0330

    beginning detail post page

--- blog/templatetags/blogg_tags.py
+++ blog/templatetags/blogg_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from blog.models import Post
 from django.db.models import Count
+from taggit.models import Tag
 
 register = template.Library()
 
@@ -8,16 +9,29 @@ register = template.Library()
 def total_posts():
     return Post.published.count()
 
-@register.inclusion_tag('blog/post/latest_posts.html')
-def show_latest_posts(count=5):
+# @register.inclusion_tag('blog/post/latest_posts.html')
+# def show_latest_posts(count=5):
+#     latest_posts = Post.published.order_by('-publish')[:count]
+#     return {'latest_posts': latest_posts}
+
+@register.simple_tag
+def show_latest_posts(count=3):
     latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts': latest_posts}
+    return latest_posts
+    return latest_posts
+    return latest_posts
 
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
-        )
+        )[:count]
+
+@register.simple_tag
+def get_All_Tags(count=10):
+    tag_names = [tag for tag in Tag.objects.all()]
+    tags=tag_names[:count]
+    return tags
 
 from django.utils.safestring import mark_safe
 import markdown
--- blog/views.py
+++ blog/views.py
@@ -79,7 +79,8 @@ def post_detail(request, year, month, day, post):
                    'comments': comments,
                    'new_comment': new_comment,
                    'comment_form': comment_form,
-                   'similar_posts': similar_posts},
+                   'similar_posts': similar_posts,
+                   },
                     using=None)
 
 def post_share(request, post_id):
@@ -111,6 +112,7 @@ def post_search(request):
     form = SearchForm()
     query = None
     results = []
+
     if 'query' in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
@@ -122,4 +124,5 @@ def post_search(request):
                   'blog/post/search.html',
                   {'form': form,
                    'query': query,
-                   'results': results})
+                   'results': results,
+                   })
